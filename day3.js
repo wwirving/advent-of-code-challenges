75d2d10508710f09fe7d5bc7fc7ecf53569c6e63
Will Irving <wjnirving@gmail.com>
2021-03-10 21:11:55 +0000

    day3 complete!

--- day3.js
+++ day3.js
@@ -3,21 +3,35 @@ var input = fs.readFileSync("./hashforest.txt").toString('utf-8');
 
 var lines = input.split("\n");
 
-var posX = 0;
-var posY = 0;
-var treeCount = 0;
+function treeCounter (xValue, yValue) {
+    var posX = 0;
+    var posY = 0;
+    var treeCount = 0;
 
-while (posY < lines.length-1){
-    posX += 3;
-    posX %= lines[0].length;
-    console.log(posX);
-    posY += 1;
-    let hash = lines[posY].charAt(posX);
-        
-    if (hash == '#') {
-            treeCount += 1;
+    while (posY < lines.length-1){
+        posX += xValue;
+        posX %= lines[0].length;
+        posY += yValue;
+        let hash = lines[posY].charAt(posX);
+            
+        if (hash == '#') {
+                treeCount += 1;
+            }
+            
         }
-        
-    }
+return treeCount;
 
-console.log(treeCount); 
+}
+
+const a = treeCounter(1,1);
+const b = treeCounter(3,1);
+const c = treeCounter(5,1);
+const d = treeCounter(7,1);
+const e = treeCounter(1,2);
+
+
+function sumA (a,b,c,d,e) {
+    return a * b * c * d * e;
+}
+
+console.log(sumA(a,b,c,d,e));
